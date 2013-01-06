cff9f5b4845541cfdab18e65ddc9d4d005db9f73
kschat <kyleschattler@gmail.com>
2013-01-06 16:33:32 -0500

    Added a function to the DatabaseHandler class that allows you to switch the fetch type rather than passing it to each fetch function in the class. Also added a addMember function to the MemberDatabaseHandler class.

--- core/utils/database/DatabaseHandler.class.php
+++ core/utils/database/DatabaseHandler.class.php
@@ -23,6 +23,14 @@ abstract class DatabaseHandler {
 	* @var boolean
 	*/
 	protected $error_handling;
+
+	/**
+	* Variable used to determine what format the returned data should be in.
+	* @access protected
+	* @var string
+	*/
+	protected $fetch_type = PDO::FETCH_ASSOC;
+
 	/**
 	* Class constructor.
 	*
@@ -54,6 +62,12 @@ abstract class DatabaseHandler {
 		$this->disconnect();
 	}
 	
+	public function setFetchType($type) {
+		if(is_numeric($type)) {
+			$this->fetch_type = $type;
+		}
+	}
+
 	/**
 	* Function used to execute a query against the database.
 	*
@@ -63,15 +77,14 @@ abstract class DatabaseHandler {
 	*
 	* @param string $query query string
 	* @param array $parameters variables that should be injected into the query
-	* @param PDO constant $fetch_type determines how the return array should be formatted
 	* @return array an array containing the information returned from the database
 	*/
-	public function executeQuery($query, array $parameters = array(), $fetch_type = PDO::FETCH_ASSOC) {
+	public function executeQuery($query, array $parameters = array()) {
 		$result = array();
 
 		$sth = $this->handle->prepare($query);
 
-		foreach ($parameters as $key => $value) {
+		foreach($parameters as $key => $value) {
 			if(is_numeric($value)) {
 				$sth->bindValue($key, (int)trim($value), PDO::PARAM_INT);
 			}
@@ -81,7 +94,7 @@ abstract class DatabaseHandler {
 		}
 		
 		if($sth->execute()) {
-			while($row = $sth->fetch($fetch_type)) {
+			while($row = $sth->fetch($this->fetch_type)) {
 				array_push($result, $row);
 			}
 		}
@@ -103,7 +116,16 @@ abstract class DatabaseHandler {
 	public function executeUpdate($query, array $parameters = array()) {
 		$sth = $this->handle->prepare($query);
 		
-		return $sth->execute($parameters);
+		foreach($parameters as $key => $value) {
+			if(is_numeric($value)) {
+				$sth->bindValue($key, (int)trim($value), PDO::PARAM_INT);
+			}
+			else {
+				$sth->bindValue($key, trim($value), PDO::PARAM_STR);
+			}
+		}
+
+		return $sth->execute();
 	}
 
 	/**
--- core/utils/database/MemberDatabaseHandler.class.php
+++ core/utils/database/MemberDatabaseHandler.class.php
@@ -31,18 +31,20 @@ class MemberDatabaseHandler extends DatabaseHandler {
 	/**
 	* Function used to execute a query against the database.
 	*
-	* This function takes a query string, an array of parameters, and a fetch type as parameters.
-	* The function attempts to execute the query using the parameters given to the function. If the
-	* query can't be executed an empty array is returned. 
+	* This function takes an id (wildcards are accepted), an offset, and an amount of members to grab
+	* as paramters. It then queries the members table based on the information provided and returns an
+	* array containing the result of the query. 
 	*
-	* @param string used to identify a single member or all members by default.
+	* @param string $id used to identify a single member or all members by default.
+	* @param int $offset used to determine the offset of members to grab based on their id's.
+	* @param int $amount used to determine how many members to get.
 	* @return array an array containing all fields from the members table
 	*/
 	public function getMembers($id = '%', $offset = 0, $amount = 10) {
 		$sql = 'SELECT *
 				FROM members
 				WHERE members.member_id LIKE :id
-				ORDER BY members.member_id DESC LIMIT :offset, :amount;';
+				ORDER BY members.member_id ASC LIMIT :offset, :amount;';
 
 		$params = array(
 				':id' 		=> $id,
@@ -51,5 +53,34 @@ class MemberDatabaseHandler extends DatabaseHandler {
 		);
 		
 		return $this->executeQuery($sql, $params);
+	}
+
+
+	/**
+	* Function used to add a member of the members table.
+	*
+	* This function takes a first name, last name, email address, and a password as parameters.
+	* It queries the database to insert a row with the information passed to the function. If the
+	* new member was added to the members table successfully it returns true; false otherwise. 
+	*
+	* @param string $fName contains the new members first name.
+	* @param string $lName contains the new members last name.
+	* @param string $email contains the new members email.
+	* @param string $password contains the new members password.
+	* @return boolean returns true if the member as added successfully; false otherwise.
+	*/
+	public function addMember($fName, $lName, $email, $password) {
+		$sql = 'INSERT INTO members
+				VALUES(:id, :fname, :lname, :email, :password);';
+
+		$params = array(
+				':id'		=> '',
+				':fname'	=> $fName,
+				':lname'	=> $lName,
+				':email'	=> $email,
+				':password'	=> $password
+		);
+
+		return $this->executeUpdate($sql, $params);
 	}
 }
